730f093903c4217b0cb5550c016c49f5be44b23b
narike avenant <narike@gmail.com>
2024-10-28 16:31:35 +0200

    Add LasatGiftData class

---
+++ NPCInfo/LastGiftData.cs
@@ -0,0 +1,47 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPCInfo
+{
+    public class LastGiftData
+    {
+        public Dictionary<string, string> LastGifts { get; set; } = new Dictionary<string, string>();
+
+        public void LoadData(IModHelper helper)
+        {
+            // Load or initialize last gift data
+            var loadedData = helper.Data.ReadSaveData<LastGiftData>("LastGiftData");
+            LastGifts = loadedData?.LastGifts ?? new Dictionary<string, string>();
+        }
+
+        public void SaveData(IModHelper helper)
+        {
+            // Save last gift data
+            helper.Data.WriteSaveData("LastGiftData", this);
+        }
+
+        public void UpdateLastGift(string npcName, string itemId)
+        {
+            LastGifts[npcName] = itemId;
+        }
+
+        private LastGiftData() { }
+
+        private static LastGiftData instance;
+        public static LastGiftData Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LastGiftData();
+                }
+                return instance;
+            }
+        }
+    }
+}
